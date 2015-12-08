89a58441cfc4399fb89ec1942cf381fdc5d5ac28
Uzpykes <validolislt@gmail.com>
2015-12-08 17:19:42 +0200

    HadamardMatrixGenerator

--- KT/src/MatrixUtilities.java
+++ KT/src/MatrixUtilities.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 
 public class MatrixUtilities {
 	
+	static int[][] H = {{1, 1}, {1, -1}};
+	
 	public static int[] multiplyByInt(int multiplier, int[] vector) {
 		if (multiplier == 0) 
 			Arrays.fill(vector, 0);
@@ -19,4 +21,33 @@ public class MatrixUtilities {
 			result[i] = multiplyByInt(vector[i], matrix[i]);
 		return result;
 	}
+	
+	
+	public static int[][] kronecherProduct(int[][] A, int [][]B) {
+		int[][] result = new int[A.length*B.length][A[0].length*B[0].length];
+		
+		for (int i = 0; i < result.length; i+=B.length)
+			for (int j = 0; j < result[0].length; j+=B[0].length)
+				for(int a = 0; a < B.length; a++)
+					for(int b = 0; b < B[0].length; b++)
+						result[i+a][j+b] = A[i/B.length][j/B[0].length]*B[a][b];
+		
+		return result;	
+	}
+	
+	
+	public static int[][] identityMatrix(int size) {
+		int[][] result = new int[size][size];
+		for (int i = 0; i < size; i++)
+			result[i][i] = 1;
+		return result;
+	}
+	
+	public static int[][] hadamardMatrix(int i, int m) {
+		
+		return MatrixUtilities.kronecherProduct(MatrixUtilities.kronecherProduct(MatrixUtilities.identityMatrix((int) Math.pow(2, m-i)), H), MatrixUtilities.identityMatrix((int) Math.pow(2, i-1)));
+	
+	}
+	
+	
 }
--- KT/src/main.java
+++ KT/src/main.java
@@ -6,26 +6,18 @@ public class main {
 	public static void main(String args[]) {
 		e = new Encoder(3);
 		e.m = new int[]{0, 1, 1, 0};
+		e.Encode();
+
+		int[] distored = ChannelSimulation.simulateNoise(0.5, e.encoded);
 		
-		for (int i = 0; i < e.matrix.length; i++) {
-			for (int j = 0; j < e.matrix[i].length; j++)
-				System.out.print(e.matrix[i][j] + "");
+		
+		int[][] k = MatrixUtilities.hadamardMatrix(1, 3);
+		
+		for (int i = 0; i < k.length; i++) {
+			for (int j = 0; j < k[i].length; j++)
+				System.out.print(k[i][j] + " ");
 			System.out.print("\n");
 		}
-		
-		e.Encode();
-		
-		for (int i = 0; i < e.encoded.length; i++) {
-			   System.out.print(e.encoded[i] + " ");
-			}
-		System.out.println();
-		int[] distored = ChannelSimulation.simulateNoise(0.5, e.encoded);
-				
-		for (int i = 0; i < distored.length; i++) {
-		   System.out.print(distored[i] + " ");
-		}
-		
-		
 	}
 	
 }
